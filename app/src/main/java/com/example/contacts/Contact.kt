16a1421574e9ea2b254e456a4cdcf9512c23b2a8
Anton Gordeev <antongordeev9505@gmail.com>
2021-11-13 11:05:53 +0300

    Last commit

--- app/src/main/java/com/example/contacts/Contact.kt
+++ app/src/main/java/com/example/contacts/Contact.kt
@@ -1,15 +1,9 @@
 package com.example.contacts
 
-import java.io.Serializable
-
 
 data class Contact (
     var firstName : String,
     var lastName : String,
     var number : String,
     val id : String,
-) : Serializable {
-    override fun toString(): String {
-        return firstName
-    }
-}
+)
